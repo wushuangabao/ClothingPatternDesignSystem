0d032d3e10eb908830b0988d701d4823399e5b9b
无双BaOY_WHA <445395697@qq.com>
2019-10-12 11:18:29 +0800

    Update mypainter.cpp

--- rules/mypainter.cpp
+++ rules/mypainter.cpp
@@ -145,11 +145,11 @@ void MyPainter::curve(QList<QPointF> points, QPointF firstCtrlPoint, QPointF las
             myPath->moveTo(points.at(0));
         while(i<=num){
             addCtrlPoints(points.at(0),points.at(1),points.at(2),ctrlPoints);
-            myPath->cubicTo(ctrlPoints->takeAt(0),ctrlPoints->takeAt(0),points.at(1)); //绘制到第2个端点的三次贝塞尔曲线
+            myPath->cubicTo(ctrlPoints->takeAt(0),ctrlPoints->takeAt(1),points.at(1)); //绘制到第2个端点的三次贝塞尔曲线
             points.takeAt(0); //删除第1个端点
             i++;
         }
-        myPath->cubicTo(ctrlPoints->takeAt(0),ctrlPoints->takeAt(0),points.at(1));
+        myPath->cubicTo(ctrlPoints->takeAt(0),ctrlPoints->takeAt(1),points.at(1));
     }
     ctrlPoints->clear();
     delete ctrlPoints;
